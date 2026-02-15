ff9b371545cbde3e6bea9281ac5efc6ce5f7f1ac
Daksh Phalswal <dakshphalswalofficial@gmail.com>
2026-02-15 23:39:26 +0530

    day15_dsa

---
+++ Day 15 DSA/Q1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    int i, j;
+    int sum = 0;
+
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(i = 0; i < m && i < n; i++) {
+        sum += matrix[i][i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
